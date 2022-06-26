3316861699e9cbe1a961d04d4e81eaa836bedc6a
huangkangyan <37485592@qq.com>
2022-06-26 12:30:38 +0800

    "java oop"

---
+++ src/com/hky/StudentTest.java
@@ -0,0 +1,11 @@
+package com.hky;
+
+import com.hky.pojo.Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student("201841413210", "Kylenn", "man", 23);
+        System.out.println(student);
+
+    }
+}
